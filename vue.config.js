c145be0e0e4b678489f3c080d304b423c4fbb339
lmc-2020 <lmc007358@163.com>
2020-09-22 00:41:27 +0800

    项目预览

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   outputDir: "pages",
-  publicPath: "./",
+  publicPath: "https://vue-project-lmc.oss-cn-chengdu.aliyuncs.com/pages",
   devServer: {
     host: "0.0.0.0",
     port: "8081"
